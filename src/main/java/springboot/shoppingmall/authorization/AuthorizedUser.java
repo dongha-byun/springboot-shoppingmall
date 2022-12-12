9dfeb7daf36dacfb6ec92f86b229f7819669a796
dongha-byun <byunsw4@naver.com>
2022-12-12 23:14:48 +0900

    Feat: 사용자 인증 annotation 기반 처리로 변경

--- src/main/java/springboot/shoppingmall/authorization/AuthorizedUser.java
+++ src/main/java/springboot/shoppingmall/authorization/AuthorizedUser.java
@@ -5,11 +5,17 @@ import lombok.Data;
 @Data
 public class AuthorizedUser {
     private Long id;
+    private String loginId;
 
     public AuthorizedUser() {
     }
 
     public AuthorizedUser(Long id) {
         this.id = id;
+    }
+
+    public AuthorizedUser(Long id, String loginId) {
+        this.id = id;
+        this.loginId = loginId;
     }
 }
---
+++ src/main/java/springboot/shoppingmall/authorization/argument/AuthenticationStrategyArgumentResolver.java
@@ -0,0 +1,31 @@
+package springboot.shoppingmall.authorization.argument;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+import springboot.shoppingmall.authorization.AuthenticationStrategy;
+import springboot.shoppingmall.authorization.AuthorizedUser;
+import springboot.shoppingmall.authorization.service.AuthService;
+
+@Component
+@RequiredArgsConstructor
+public class AuthenticationStrategyArgumentResolver implements HandlerMethodArgumentResolver {
+
+    private final AuthService authService;
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.hasParameterAnnotation(AuthenticationStrategy.class);
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
+                                  NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+
+        return new AuthorizedUser(20L, "testUser");
+    }
+}
