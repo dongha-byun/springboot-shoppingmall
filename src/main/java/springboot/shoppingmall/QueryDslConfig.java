33d6d3f19b149a05b896fdf8fa16c3390880b73e
dongha-byun <byunsw4@naver.com>
2023-01-13 21:14:17 +0900

    Chore: QueryDSL 설정 추가

---
+++ src/main/java/springboot/shoppingmall/QueryDslConfig.java
@@ -0,0 +1,22 @@
+package springboot.shoppingmall;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * QueryDSL Configuration
+ */
+@Configuration
+public class QueryDslConfig {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory(){
+        return new JPAQueryFactory(em);
+    }
+}
