f2c5243ba8aaf169fc61c8e0d2c7cef13bd5ca74
dongha-byun <byunsw4@naver.com>
2023-04-27 17:04:11 +0900

    chore: 판매자 페이지 port 분리

--- src/main/java/springboot/shoppingmall/WebConfig.java
+++ src/main/java/springboot/shoppingmall/WebConfig.java
@@ -8,10 +8,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    private static final String[] WHITE_LIST = {
+            "http://localhost:3000", "http://local.shoppingmall.com:3000",
+            "http://localhost:3001", "http://local.shoppingmall.com:3001"
+    };
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://local.shoppingmall.com:3000")
+                .allowedOrigins(WHITE_LIST)
                 .allowedMethods(
                         HttpMethod.GET.name(),
                         HttpMethod.POST.name(),
@@ -20,6 +25,5 @@ public class WebConfig implements WebMvcConfigurer {
                         HttpMethod.OPTIONS.name()
                 )
                 .maxAge(1800); // 30min
-
     }
 }
