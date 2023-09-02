875d019449fcb9c26f161b9ce00b15daece21d21
dongha-byun <byunsw4@naver.com>
2023-09-02 19:13:11 +0900

    refactor: 용어 변경  - Authorization 에서 Authentication 으로 변경

--- src/test/java/springboot/shoppingmall/AcceptanceTest.java
+++ src/test/java/springboot/shoppingmall/AcceptanceTest.java
@@ -15,7 +15,6 @@ import java.util.Map;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.server.LocalServerPort;
@@ -31,7 +30,7 @@ import springboot.shoppingmall.user.dto.UserResponse;
 @Import({
         TestFileConfiguration.class,
         TestOrderConfig.class,
-        TestEmailAuthorizationConfig.class
+        TestEmailAuthenticationConfig.class
 })
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class AcceptanceTest {
