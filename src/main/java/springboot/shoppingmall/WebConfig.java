73c5d34ea03ee4e6ba93e3e08df9274285bb9a2e
dongha-byun <byunsw4@naver.com>
2023-11-30 17:26:38 +0900

    fix: CORS 설정 제거  - API Gateway 에서 공통 처리

--- src/main/java/springboot/shoppingmall/WebConfig.java
+++ src/main/java/springboot/shoppingmall/WebConfig.java
@@ -3,9 +3,7 @@ package springboot.shoppingmall;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springboot.shoppingmall.authorization.GatewayAuthenticationArgumentResolver;
 
@@ -14,24 +12,6 @@ import springboot.shoppingmall.authorization.GatewayAuthenticationArgumentResolv
 public class WebConfig implements WebMvcConfigurer {
 
     private final GatewayAuthenticationArgumentResolver gatewayAuthenticationArgumentResolver;
-
-    private static final String[] WHITE_LIST = {
-            "http://localhost:3000", "http://local.shoppingmall.com:3000",
-            "http://localhost:3001", "http://local.shoppingmall.com:3001"
-    };
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins(WHITE_LIST)
-                .allowedMethods(
-                        HttpMethod.GET.name(),
-                        HttpMethod.POST.name(),
-                        HttpMethod.PUT.name(),
-                        HttpMethod.DELETE.name()
-                );
-//                .maxAge(1800); // 30min
-    }
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
