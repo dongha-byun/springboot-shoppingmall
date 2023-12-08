57b53a7e0a6380659ff7547f2af666879dbeaaa7
dongha-byun <byunsw4@naver.com>
2023-12-08 19:31:32 +0900

    test: FeignClient mocking 처리

--- src/test/java/springboot/shoppingmall/coupon/application/CouponServiceTest.java
+++ src/test/java/springboot/shoppingmall/coupon/application/CouponServiceTest.java
@@ -1,24 +1,18 @@
 package springboot.shoppingmall.coupon.application;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.*;
-import static org.springframework.test.web.client.response.MockRestResponseCreators.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.when;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import java.time.LocalDateTime;
 import java.util.Arrays;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.client.MockRestServiceServer;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.client.RestTemplate;
+import springboot.shoppingmall.client.userservice.UserServiceClient;
 import springboot.shoppingmall.coupon.domain.Coupon;
 import springboot.shoppingmall.coupon.domain.CouponRepository;
 
@@ -32,22 +26,12 @@ class CouponServiceTest {
     @Autowired
     CouponRepository couponRepository;
 
-    @Autowired
-    RestTemplate restTemplate;
-
-    @Autowired
-    ObjectMapper objectMapper;
-
-    MockRestServiceServer mockRestServiceServer;
-
-    @BeforeEach
-    void beforeEach() {
-        mockRestServiceServer = MockRestServiceServer.createServer(restTemplate);
-    }
+    @MockBean
+    UserServiceClient userServiceClient;
 
     @Test
     @DisplayName("쿠폰 생성 - 특정 회원등급 이상의 회원들에게 쿠폰을 발급한다.")
-    void create_coupon_for_user() throws JsonProcessingException {
+    void create_coupon_for_user() {
         // 단골회원(REGULAR) 등급 이상인 회원들에게
         // 사용기한이 2023-05-28 ~ 2023-07-28 인
         // 할인율 7%의 쿠폰을 발급해준다.
@@ -61,8 +45,9 @@ class CouponServiceTest {
                 name, fromDate, toDate, "REGULAR", discountRate, partnersId
         );
 
-        String resultContent = objectMapper.writeValueAsString(Arrays.asList(10L, 20L, 30L));
-        mockRestServiceServerWithGetUsersAboveTargetGrade(couponCreateDto.getGrade(), resultContent);
+        when(userServiceClient.getUserIdsAboveTheGrade(any())).thenReturn(
+                Arrays.asList(100L, 200L, 300L)
+        );
 
         // when
         Long couponId = couponService.create(couponCreateDto);
@@ -72,18 +57,5 @@ class CouponServiceTest {
         assertThat(savedCoupon).isNotNull();
         assertThat(savedCoupon.getId()).isEqualTo(couponId);
         assertThat(savedCoupon.getUserCoupons()).hasSize(3);
-    }
-
-    private void mockRestServiceServerWithGetUsersAboveTargetGrade(String targetGrade, String resultContent) {
-        mockRestServiceServer
-                .expect(
-                        requestTo("/users/aboveGrade?targetGrade="+targetGrade)
-                )
-                .andExpect(method(HttpMethod.GET))
-                .andRespond(
-                        withStatus(HttpStatus.OK)
-                                .contentType(MediaType.APPLICATION_JSON)
-                                .body(resultContent)
-                );
     }
 }
