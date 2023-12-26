bea7cbacc9f5a06f5949ee79a1bb2cf44a7c5e8a
dongha-byun <byunsw4@naver.com>
2023-12-26 23:34:55 +0900

    fix: restTemplate DI 오류로 인해 Bean 명시

--- src/main/java/springboot/shoppingmall/WebConfig.java
+++ src/main/java/springboot/shoppingmall/WebConfig.java
@@ -2,7 +2,9 @@ package springboot.shoppingmall;
 
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springboot.shoppingmall.authorization.GatewayAuthenticationArgumentResolver;
@@ -16,5 +18,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(gatewayAuthenticationArgumentResolver);
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
