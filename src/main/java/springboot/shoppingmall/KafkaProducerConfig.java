9ec9d846e605e768ffd85dccae75a8e2d166945c
dongha-byun <byunsw4@naver.com>
2024-01-19 20:21:10 +0900

    chore: kafka 설정 변경  - config 파일에서 yml 파일로 변경  - 한 애플리케이션 내에 producer/consumer 동시 처리

---
+++ src/main/java/springboot/shoppingmall/KafkaProducerConfig.java
@@ -0,0 +1,37 @@
+package springboot.shoppingmall;
+
+import static org.apache.kafka.clients.producer.ProducerConfig.BOOTSTRAP_SERVERS_CONFIG;
+import static org.apache.kafka.clients.producer.ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG;
+import static org.apache.kafka.clients.producer.ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+//@Configuration
+public class KafkaProducerConfig {
+
+    private static final String KAFKA_SERVER_URL = "127.0.0.1:9872";
+
+    @Bean
+    public ProducerFactory<String, String> producerFactory() {
+        Map<String, Object> properties = new HashMap<>();
+
+        properties.put(BOOTSTRAP_SERVERS_CONFIG, "localhost:9872");
+        properties.put(KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        properties.put(VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+
+        return new DefaultKafkaProducerFactory<>(properties);
+    }
+
+    @Bean
+    public KafkaTemplate<String, String> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+
+}
