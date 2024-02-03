33cb4f2e2eff8cab1356d086ae6b9dfe344a6a3b
dongha-byun <byunsw4@naver.com>
2024-02-03 23:25:42 +0900

    refactor: 구매 중단 처리 수정

--- src/main/java/springboot/shoppingmall/order/application/OrderItemResolutionService.java
+++ src/main/java/springboot/shoppingmall/order/application/OrderItemResolutionService.java
@@ -1,11 +1,13 @@
 package springboot.shoppingmall.order.application;
 
+import static springboot.shoppingmall.order.domain.OrderItemResolutionType.*;
+
 import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.client.RestTemplate;
-import springboot.shoppingmall.order.domain.Order;
+import springboot.shoppingmall.client.couponservice.CouponServiceClient;
+import springboot.shoppingmall.client.payservice.PayServiceClient;
 import springboot.shoppingmall.order.domain.OrderFinder;
 import springboot.shoppingmall.order.domain.OrderItem;
 import springboot.shoppingmall.order.domain.OrderItemResolutionHistory;
@@ -13,17 +15,16 @@ import springboot.shoppingmall.order.domain.OrderItemResolutionHistoryRepository
 import springboot.shoppingmall.order.domain.OrderItemResolutionType;
 import springboot.shoppingmall.pay.domain.PayHistory;
 import springboot.shoppingmall.pay.domain.PayHistoryRepository;
-import springboot.shoppingmall.pay.type.kakakopay.web.cancel.KakaoPayCancelRequest;
-import springboot.shoppingmall.pay.type.kakakopay.web.cancel.KakaoPayCancelResponse;
 
 @RequiredArgsConstructor
 @Transactional
 @Service
 public class OrderItemResolutionService {
     private final OrderItemResolutionHistoryRepository repository;
-    private final OrderFinder orderFinder;
     private final PayHistoryRepository payHistoryRepository;
-    private final RestTemplate restTemplate;
+    private final OrderFinder orderFinder;
+    private final CouponServiceClient couponServiceClient;
+    private final PayServiceClient payServiceClient;
 
     public Long saveResolutionHistory(
             Long userId, Long orderItemId, OrderItemResolutionType resolutionType,
@@ -49,34 +50,23 @@ public class OrderItemResolutionService {
                 )
         );
 
-        // 여기서 두 가지 이벤트가 발생해야함. 비동기로 - 일단 동기로 개발해보기
-        // 1. PG사 결제 취소 요청
-        //cancelPayment(orderItem);
+        if(resolutionType == CANCEL) {
+            // 여기서 두 가지 이벤트가 발생해야함. 비동기로 - 일단 동기로 개발해보기
+            // 1. PG사 결제 취소 요청
+            PayHistory payHistory = payHistoryRepository.findByOrderId(orderItem.getOrder().getId())
+                    .orElseThrow(
+                            () -> new IllegalArgumentException("거래내역 조회에 실패했습니다.")
+                    );
+            payServiceClient.cancel(payHistory.getTid(), payHistory.getPayType().name(), orderItem.getRealPayPrice());
 
-        // 2. 쿠폰 미사용 처리
-        // kafka or spring domain event
+            // 2. 쿠폰 미사용 처리
+            // spring domain event 를 적용해서 AbstractAggregateRoot 를 쓰려했지만, 추상클래스라 entity 에 사용할 수 가 없다.
+            // 이미 BaseEntity 가 들어가 있기 때문. 차라리 다른 비동기 이벤트 방식을 써야겠는데, 만만한게 카프카 ?
+            // 여기선 카프카 이벤트를 생성하고 보내는 식으로 해보자, 어차피 couponId 만 넘어가면 됨
+            // eventPublisher -> eventListener
+            couponServiceClient.recoveryCoupon(orderItem.getUsedUserCouponId());
+        }
 
         return resolutionHistory.getId();
     }
-
-    private void cancelPayment(OrderItem orderItem) {
-        Order order = orderItem.getOrder();
-        PayHistory payHistory = payHistoryRepository.findByOrderId(order.getId())
-                .orElseThrow(
-                        () -> new IllegalArgumentException("거래내역 조회에 실패했습니다.")
-                );
-
-        KakaoPayCancelRequest kakaoPayCancelRequest = new KakaoPayCancelRequest(
-                payHistory.getTid(),
-                orderItem.getRealPayPrice(),
-                0
-        );
-
-        restTemplate.postForObject(
-                "/pay/" + payHistory.getPayType() + "/cancel",
-                kakaoPayCancelRequest,
-                KakaoPayCancelResponse.class
-        );
-    }
-
 }
