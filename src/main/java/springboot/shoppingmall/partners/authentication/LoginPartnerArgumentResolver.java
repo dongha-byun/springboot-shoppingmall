b314686f20b61b2c3dd650555a249e772a54e78a
dongha-byun <byunsw4@naver.com>
2024-02-14 16:21:56 +0900

    feat: 파트너즈 인증 관련  코드 추가  - 인증 처리를 위한 ArgumentResolver 추가

--- src/main/java/springboot/shoppingmall/partners/authentication/LoginPartnerArgumentResolver.java
+++ src/main/java/springboot/shoppingmall/partners/authentication/LoginPartnerArgumentResolver.java
@@ -4,13 +4,18 @@ import javax.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.MethodParameter;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
+import springboot.shoppingmall.authorization.GatewayConstants;
 
 @Slf4j
 @RequiredArgsConstructor
+@Component
 public class LoginPartnerArgumentResolver implements HandlerMethodArgumentResolver {
 
     @Override
@@ -26,7 +31,11 @@ public class LoginPartnerArgumentResolver implements HandlerMethodArgumentResolv
             throw new IllegalStateException("네트워크에 문제가 발생했습니다. 잠시 후, 다시 시도해주세요.");
         }
 
-        String token = "token";
-        return new AuthorizedPartner();
+        String xGatewaySubject = request.getHeader(GatewayConstants.GATEWAY_HEADER);
+        if(!StringUtils.hasText(xGatewaySubject)) {
+            throw new IllegalArgumentException("Gateway Subject is not exists!");
+        }
+
+        return new AuthorizedPartner(Long.parseLong(xGatewaySubject));
     }
 }
