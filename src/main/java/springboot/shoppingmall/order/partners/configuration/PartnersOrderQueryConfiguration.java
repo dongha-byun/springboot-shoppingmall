fe7f0def619383b8dc90d0724a23cc0edbf1f5b4
dongha-byun <byunsw4@naver.com>
2024-03-21 23:54:01 +0900

    chore: 미사용 bean 제거

--- src/main/java/springboot/shoppingmall/order/partners/configuration/PartnersOrderQueryConfiguration.java
+++ src/main/java/springboot/shoppingmall/order/partners/configuration/PartnersOrderQueryConfiguration.java
@@ -1,19 +1,14 @@
 package springboot.shoppingmall.order.partners.configuration;
 
-import java.util.HashMap;
-import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
 import springboot.shoppingmall.order.application.OrderUserInterfaceService;
-import springboot.shoppingmall.order.partners.domain.PartnersOrderQueryRepository;
-import springboot.shoppingmall.order.partners.domain.PartnersOrderQueryType;
 import springboot.shoppingmall.order.partners.application.PartnersCancelOrderQueryService;
-import springboot.shoppingmall.order.partners.application.PartnersEndOrderQueryService;
 import springboot.shoppingmall.order.partners.application.PartnersDeliveryOrderQueryService;
-import springboot.shoppingmall.order.partners.application.PartnersOrderQueryService;
+import springboot.shoppingmall.order.partners.application.PartnersEndOrderQueryService;
 import springboot.shoppingmall.order.partners.application.PartnersReadyOrderQueryService;
+import springboot.shoppingmall.order.partners.domain.PartnersOrderQueryRepository;
 
 @RequiredArgsConstructor
 @Configuration
@@ -21,16 +16,6 @@ public class PartnersOrderQueryConfiguration {
 
     private final PartnersOrderQueryRepository repository;
     private final OrderUserInterfaceService orderUserInterfaceService;
-
-//    public Map<PartnersOrderQueryType, PartnersOrderQueryService> partnersOrderQueryServiceMap() {
-//        Map<PartnersOrderQueryType, PartnersOrderQueryService> map = new HashMap<>();
-//        map.put(PartnersOrderQueryType.READY, partnersReadyOrderQueryService());
-//        map.put(PartnersOrderQueryType.DELIVERY, partnersDeliveryOrderQueryService());
-//        map.put(PartnersOrderQueryType.END, partnersEndOrderQueryService());
-//        map.put(PartnersOrderQueryType.CANCEL, partnersCancelOrderQueryService());
-//
-//        return map;
-//    }
 
     @Bean
     public PartnersReadyOrderQueryService partnersReadyOrderQueryService() {
