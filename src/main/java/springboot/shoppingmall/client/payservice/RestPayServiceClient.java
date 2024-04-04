4d4e9ee265d93d8e19ce36d347c8c60cd2491393
dongha-byun <byunsw4@naver.com>
2024-04-04 23:49:52 +0900

    refactor: 거래내역 조회 위치 변경

--- src/main/java/springboot/shoppingmall/client/payservice/RestPayServiceClient.java
+++ src/main/java/springboot/shoppingmall/client/payservice/RestPayServiceClient.java
@@ -3,6 +3,8 @@ package springboot.shoppingmall.client.payservice;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+import springboot.shoppingmall.pay.domain.PayHistory;
+import springboot.shoppingmall.pay.domain.PayHistoryRepository;
 import springboot.shoppingmall.pay.type.kakakopay.web.cancel.KakaoPayCancelRequest;
 import springboot.shoppingmall.pay.type.kakakopay.web.cancel.KakaoPayCancelResponse;
 import springboot.shoppingmall.pay.web.PayRequest;
@@ -10,19 +12,25 @@ import springboot.shoppingmall.pay.web.PayRequest;
 @RequiredArgsConstructor
 @Component
 public class RestPayServiceClient implements PayServiceClient{
+    private final PayHistoryRepository payHistoryRepository;
     private final RestTemplate restTemplate;
     private static final String PAY_URL = "http://localhost:10000/pay/";
 
     @Override
-    public void cancel(String tid, String payType, int cancelAmount) {
+    public void cancel(Long orderId, int cancelAmount) {
+        PayHistory payHistory = payHistoryRepository.findByOrderId(orderId)
+                .orElseThrow(
+                        () -> new IllegalArgumentException("거래내역 조회 실패")
+                );
+
         KakaoPayCancelRequest kakaoPayCancelRequest = new KakaoPayCancelRequest(
-                tid,
+                payHistory.getTid(),
                 cancelAmount,
                 0
         );
-        PayRequest<KakaoPayCancelRequest> payRequest = new PayRequest<>(payType, kakaoPayCancelRequest);
+        PayRequest<KakaoPayCancelRequest> payRequest = new PayRequest<>(payHistory.getPayType().name(), kakaoPayCancelRequest);
 
-        String uri = PAY_URL + payType + "/cancel";
+        String uri = PAY_URL + payHistory.getPayType().name() + "/cancel";
         restTemplate.postForObject(
                 uri,
                 payRequest,
--- src/main/java/springboot/shoppingmall/order/handler/OrderCanceledEventHandler.java
+++ src/main/java/springboot/shoppingmall/order/handler/OrderCanceledEventHandler.java
@@ -5,24 +5,21 @@ import org.springframework.stereotype.Component;
 import springboot.shoppingmall.client.couponservice.CouponServiceClient;
 import springboot.shoppingmall.client.payservice.PayServiceClient;
 import springboot.shoppingmall.order.domain.OrderItem;
-import springboot.shoppingmall.pay.domain.PayHistory;
-import springboot.shoppingmall.pay.domain.PayHistoryRepository;
 
 @RequiredArgsConstructor
 @Component
 public class OrderCanceledEventHandler {
-    private final PayHistoryRepository payHistoryRepository;
     private final PayServiceClient payServiceClient;
     private final CouponServiceClient couponServiceClient;
 
     public void handle(OrderItem orderItem) {
+        // 1번과 2번이 비동기로 처리될 방법?
+        // Async 로 돌리자. public 으로 파서
+        // 1개의 메세지를 발행하고 2개의 consumer 가 가져갈 수 있나?
+
         // 여기서 두 가지 이벤트가 발생해야함. 비동기로 - 일단 동기로 개발해보기
         // 1. PG사 결제 취소 요청
-        PayHistory payHistory = payHistoryRepository.findByOrderId(orderItem.getOrder().getId())
-                .orElseThrow(
-                        () -> new IllegalArgumentException("거래내역 조회에 실패했습니다.")
-                );
-        payServiceClient.cancel(payHistory.getTid(), payHistory.getPayType().name(), orderItem.getRealPayPrice());
+        payServiceClient.cancel(orderItem.getOrder().getId(), orderItem.getRealPayPrice());
 
         // 2. 쿠폰 미사용 처리
         // spring domain event 를 적용해서 AbstractAggregateRoot 를 쓰려했지만, 추상클래스라 entity 에 사용할 수 가 없다.
