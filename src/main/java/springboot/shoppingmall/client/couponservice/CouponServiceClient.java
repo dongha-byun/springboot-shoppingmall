9a615cbaa7e23d2501987c1b0f616eba0d5fb0b2
dongha-byun <byunsw4@naver.com>
2024-04-12 18:07:26 +0900

    refactor: 주문 취소 시 수행되어야 하는 부가로직 비동기 처리

--- src/main/java/springboot/shoppingmall/client/couponservice/CouponServiceClient.java
+++ src/main/java/springboot/shoppingmall/client/couponservice/CouponServiceClient.java
@@ -1,6 +1,9 @@
 package springboot.shoppingmall.client.couponservice;
 
+import org.springframework.scheduling.annotation.Async;
+
 public interface CouponServiceClient {
 
+    @Async
     void recoveryCoupon(Long userCouponId);
 }
--- src/main/java/springboot/shoppingmall/client/payservice/PayServiceClient.java
+++ src/main/java/springboot/shoppingmall/client/payservice/PayServiceClient.java
@@ -1,6 +1,9 @@
 package springboot.shoppingmall.client.payservice;
 
+import org.springframework.scheduling.annotation.Async;
+
 public interface PayServiceClient {
 
+    @Async
     void cancel(Long orderId, int cancelAmount);
 }
